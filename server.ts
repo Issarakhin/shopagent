56c06ea5c0ee1f29b4a14e0957491d4d7c7e4d25
Issarakhin <83199507+Issarakhin@users.noreply.github.com>
2026-08-16 16:43:21 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -58,15 +58,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 // Enable CORS for ease of access if accessed from external services
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Admin-Key, X-Admin-User, X-Admin-Role');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 // Paths for persistent JSON database store
 // Using process.cwd() ensures compatibility when running either locally or in a container/Heroku
